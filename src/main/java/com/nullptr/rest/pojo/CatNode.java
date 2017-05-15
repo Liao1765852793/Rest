1e639043820ad51865ad013ca8bcc3cbc5d5e4e7
Liao1765852793 <1765852793@qq.com>
2017-05-15 23:21:27 +0800

    Rest项目搭建    引入Web.xml  springmvc.xml 整合框架
       pom.xml文件修改，增加部分依赖项
    
       引入category.js文件，测试portal项目中通过使用JSonP做JS的跨域操作，在portal项目中直接使用链接取得category.js的内容。
       注：js跨域这里是采用读取js的片段代码进行的

---
+++ src/main/java/com/nullptr/rest/pojo/CatNode.java
@@ -0,0 +1,44 @@
+package com.nullptr.rest.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/**
+ * Created by Nullptr on 2017/5/15.
+ * 分类节点Item
+ */
+public class CatNode {
+    @JsonProperty(value = "u")
+    private String url;
+    @JsonProperty(value = "n")
+    private String name;
+    @JsonProperty(value = "i")
+    private List items;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List getItems() {
+        return items;
+    }
+
+    public void setItems(List items) {
+        this.items = items;
+    }
+
+
+}
---
+++ src/main/java/com/nullptr/rest/pojo/ItemCatResult.java
@@ -0,0 +1,18 @@
+package com.nullptr.rest.pojo;
+
+import java.util.List;
+
+/**
+ * Created by Nullptr on 2017/5/15.
+ */
+public class ItemCatResult {
+    private List data;
+
+    public List getData() {
+        return data;
+    }
+
+    public void setData(List data) {
+        this.data = data;
+    }
+}
